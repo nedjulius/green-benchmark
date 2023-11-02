fbac14ad42711fb93aeff62e7ef31da59aafc348
nedjulius <julius.nedzinskas@gmail.com>
2023-11-02 00:24:00 -0400

    Add services and the path

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from services import search_ratings
 
 app = Flask(__name__)
 
@@ -7,6 +8,15 @@ app = Flask(__name__)
 @app.route("/index")
 def main():
     return render_template("index.html")
+
+
+@app.route("/search/<topic>")
+def search(topic):
+    results = search_ratings(topic)
+    return {
+        "results": results,
+        "length": len(results)
+    }
 
 
 if __name__ == '__main__':
