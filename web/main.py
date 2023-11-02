75535e167042e1f8e2edf292bf3c6df141debc44
nedjulius <julius.nedzinskas@gmail.com>
2023-11-01 21:09:45 -0400

    Add templates

--- web/main.py
+++ web/main.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello world!</p>"
+@app.route("/index")
+def main():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
